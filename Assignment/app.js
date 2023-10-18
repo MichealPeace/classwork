f9c4c3eb7f17957dc836fcf68cd56a21fe367f41
MichealPeace <137876761+MichealPeace@users.noreply.github.com>
2023-10-18 12:14:19 +0300

    Add files via upload

---
+++ Assignment/app.js
@@ -0,0 +1,38 @@
+
+
+let num1 = parseFloat(prompt('enter first number'))
+let num2 = parseFloat(prompt('enter second number'))
+let num3 = parseFloat(prompt('enter Third number'))
+let mean = (num1+num2+num3)/3
+console.log(mean)
+
+let compare1= parseFloat(12)
+let compare2= parseFloat(6)
+let result =compare1>compare2
+let result1 =compare1<compare2
+let result2 =compare1==compare2
+console.log(result )
+console.log(result1)
+console.log(result2)
+
+
+let Number1 = parseFloat(8)
+
+
+let check=(Number1%2==0)
+console.log(check) 
+
+
+let firstval =7
+let secondval=firstval
+console.log(secondval) 
+
+
+let variable1= 5
+let variable2="5"
+
+let resultofcomapre1= variable1==variable2
+console.log(resultofcomapre1)
+
+let resultofcomapre2= variable1===variable2
+console.log(resultofcomapre2)
